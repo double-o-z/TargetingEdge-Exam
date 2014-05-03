5a5e2c21e4949eaf1e6e6a01a4667242994360e0
or-ohev-zion <or.ohev.zion@gmail.com>
2014-05-03 19:15:37 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exam::Application.config.secret_key_base = '8e0bc6584b6cb77acca9e4b6a5402dca7d847598bb1e5942d819f9c49f032199325a8e087ec33019ddbcc182b76eabf559fe22c21986e10e742e3de033f1bf26'
---
+++ public/javascripts/fileSystemBrowser.js
@@ -0,0 +1,4 @@
+/**
+ * Created by or on 5/3/14.
+ */
+
