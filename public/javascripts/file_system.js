943d9dac53dca3f8702d3fe9713c7b4ca1ec04a4
or-ohev-zion <or.ohev.zion@gmail.com>
2014-05-03 23:51:39 +0300

    transfered js code to external .js file in public directory.

---
+++ public/javascripts/file_system.js
@@ -0,0 +1,41 @@
+/**
+ * Created by or on 5/3/14.
+ */
+function replaceBrowserContents (elm){
+    var thisFolder = elm;
+    $.ajax({
+        url: "/contents",
+        type: "get",
+        context: thisFolder,
+        data: {'thisFolder': thisFolder},
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function successCallback (result){
+            $('p').detach();
+            var lastFolder = document.createElement("p");
+            var lastFolderText = thisFolder.substring(0, thisFolder.lastIndexOf("/"));
+            $(lastFolder).append(lastFolderText);
+            $(lastFolder).addClass('row');
+            $(lastFolder).css('color', 'green');
+            $('.contents').prepend(lastFolder);
+            for(var key in result){
+                if(result.hasOwnProperty(key)){
+                    var newContent = document.createElement("p");
+                    $(newContent).append(key);
+                    if(result[key]){
+                        $(newContent).addClass('row');
+                        $(newContent).css('color', 'blue');
+                    }
+                    $('.contents').append(newContent);
+                }
+            }
+        }
+    });
+}
+$(document).ready(function (){
+    replaceBrowserContents("/home/or/Downloads"); // change this to a local folder.
+    $('.contents').on('click', '.row', function(){
+        var thisElement = $(this).text();
+        replaceBrowserContents(thisElement);
+    })
+});
